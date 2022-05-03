888a66abab8099c777d8886b9f6adee0445c6434
krystof-hruby <98039183+krystof-hruby@users.noreply.github.com>
2022-05-03 20:27:47 +0100

    Finished writing the text

--- MatLab code/main.m
+++ MatLab code/main.m
@@ -13,27 +13,6 @@ for index = 1:10
     % Split data into training and testing sets
     testingAmount = 0.05; % 5% reserved for testing
     [data_Training, data_Testing] = splitData(data, testingAmount);
-    
-    %%%%%%%%%%%%%%%%%
-    file_PositiveTexts = fopen(fullfile('sentiment-lexicon','positive-texts2.txt'));
-    %file_NegativeTexts = fopen(fullfile('sentiment-lexicon','negative-texts2.txt'));
-    
-    textscan_PositiveTexts = textscan(file_PositiveTexts, '%s');
-    %textscan_NegativeTexts = textscan(file_NegativeTexts, '%s');
-    
-    positiveTexts = string(textscan_PositiveTexts{1});
-    %negativeTexts = string(textscan_NegativeTexts{1});
-
-    fclose all; % Close all files
-    
-    % Create a Text:Label table
-    textsAll = [positiveTexts]%; negativeTexts];
-    labels = categorical(nan(numel(textsAll),1));
-    labels(1:numel(positiveTexts)) = "Positive";
-    labels(numel(positiveTexts)+1:end) = "Negative";
-    
-    data_Testing = table(textsAll, labels, 'VariableNames', {'Text','Label'});
-    %%%%%%%%%%%%%%%%%%%
 
     % Create a sentiment classifier (class)
     sentimentClassifier = SentimentClassifier;
